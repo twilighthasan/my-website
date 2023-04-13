9b94e1514351d512e6b26810b387db93ff9ab4c4
twilighthasan <hasanshadiev96@mail.ru>
2023-04-14 01:09:01 +0300

    Add delete and edit name features

--- scripts.js
+++ scripts.js
@@ -1,31 +1,51 @@
-// Получаем элементы страницы
-const nameInput = document.getElementById('nameInput');
-const addNameBtn = document.getElementById('addName');
-const nameList = document.getElementById('nameList');
+document.getElementById("addBtn").addEventListener("click", addName);
+document.getElementById("sortBtn").addEventListener("click", sortNames);
+document.getElementById("deleteBtn").addEventListener("click", deleteName);
+document.getElementById("editBtn").addEventListener("click", editName);
 
-// Создаем массив для хранения имен
-let names = [];
-
-// Функция для добавления имени
 function addName() {
-    const name = nameInput.value;
-    names.push(name);
-    names.sort();
-    displayNames();
-    nameInput.value = '';
+    const name = document.getElementById("nameInput").value;
+    const listItem = document.createElement("li");
+    listItem.innerText = name;
+    document.getElementById("namesList").appendChild(listItem);
 }
 
-// Функция для отображения имен
-function displayNames() {
-    nameList.innerHTML = '';
-    names.forEach(name => {
-        const listItem = document.createElement('li');
-        listItem.textContent = name;
-        nameList.appendChild(listItem);
-    });
+function sortNames() {
+    const namesList = document.getElementById("namesList");
+    const names = Array.from(namesList.getElementsByTagName("li"));
+
+    names.sort((a, b) => a.innerText.localeCompare(b.innerText));
+
+    while (namesList.firstChild) {
+        namesList.removeChild(namesList.firstChild);
+    }
+
+    for (const name of names) {
+        namesList.appendChild(name);
+    }
 }
 
-// Добавляем обработчик события клика на кнопку
-addNameBtn.addEventListener('click', addName);
+function deleteName() {
+    const nameToDelete = document.getElementById("nameInput").value;
+    const namesList = document.getElementById("namesList");
 
+    for (let i = 0; i < namesList.children.length; i++) {
+        if (namesList.children[i].innerText === nameToDelete) {
+            namesList.removeChild(namesList.children[i]);
+            break;
+        }
+    }
+}
 
+function editName() {
+    const oldName = document.getElementById("nameInput").value;
+    const newName = document.getElementById("newNameInput").value;
+    const namesList = document.getElementById("namesList");
+
+    for (let i = 0; i < namesList.children.length; i++) {
+        if (namesList.children[i].innerText === oldName) {
+            namesList.children[i].innerText = newName;
+            break;
+        }
+    }
+}
